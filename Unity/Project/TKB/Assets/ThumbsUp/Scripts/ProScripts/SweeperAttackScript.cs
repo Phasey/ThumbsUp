4db1b6b6897b682cb4bad39a5c061960f7491962
Phasey <phasey.angus@gmail.com>
2017-10-11 11:44:30 +1100

    Redoing the unity project

---
+++ Unity/Project/TKB/Assets/ThumbsUp/Scripts/ProScripts/SweeperAttackScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class SweeperAttackScript : MonoBehaviour
+{
+    public XboxController Controller;
+
+    // Use this for initialization
+    void Awake()
+    {
+       
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        SweeperAttack();
+    }
+
+    private void SweeperAttack()
+    {
+        float attackButton = XCI.GetAxis(XboxAxis.RightTrigger, Controller);
+
+        if (attackButton > 0.15f)
+        {
+            print("Sweeper!!");
+        }
+    }
+}
