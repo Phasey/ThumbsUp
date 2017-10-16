d7d78c9f65641505e7d369091a7123824dea4901
Matthew Le Nepveu <matthew@lenepveu.com>
2017-10-16 14:00:29 +1100

    SVN added the Sweeper and Striker Special Attack scripts.

---
+++ Unity/Project/TKB/Assets/ThumbsUp/Scripts/ProScripts/StrikerSpecial.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using XboxCtrlrInput;
+
+public class StrikerSpecial : MonoBehaviour
+{
+    public int hitForce = 10;
+    public float specialTimer = 0.5f;
+
+    private bool attacking = false;
+    private float resetTimer;
+
+    // Allows access to xbox controller buttons
+    private XboxController Controller;
+
+    //------------------------------------------------------------
+    // Function is called when script first runs
+    //------------------------------------------------------------
+    void Awake()
+    {
+        PlayerMove move = GetComponent<PlayerMove>();
+        Controller = move.Controller;
+        resetTimer = specialTimer;
+    }
+
+    //------------------------------------------------------------
+    // Function is called once every frame
+    //------------------------------------------------------------
+    void Update()
+    {
+        // Calls Special function every frame
+        Special();
+    }
+
+    //------------------------------------------------------------
+    // Function allows Striker to use his regular attack
+    //------------------------------------------------------------
+    private void Special()
+    {
+        bool attackButton = XCI.GetButtonDown(XboxButton.RightBumper, Controller);
+
+        if (attackButton && !attacking)
+        {
+            attacking = true;
+        }
+
+        if (attacking)
+        {
+            PlayerMove pm = gameObject.GetComponent<PlayerMove>();
+            pm.movementSpeed = 30;
+            specialTimer -= Time.deltaTime;
+
+            if (specialTimer <= 0)
+            {
+                attacking = false;
+                pm.movementSpeed = 10;
+                ResetCoolDown();
+            }
+        }
+    }
+
+    private void ResetCoolDown()
+    {
+        specialTimer = resetTimer;
+    }
+}
---
+++ Unity/Project/TKB/Assets/ThumbsUp/Scripts/ProScripts/SweeperSpecial.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using XboxCtrlrInput;
+
+public class SweeperSpecial : MonoBehaviour
+{
+    public int hitForce = 10;
+
+    // Allows access to xbox controller buttons
+    private XboxController Controller;
+    public GameObject HitBox;
+
+    //------------------------------------------------------------
+    // Function is called when script first runs
+    //------------------------------------------------------------
+    void Awake()
+    {
+        PlayerMove move = GetComponent<PlayerMove>();
+        Controller = move.Controller;
+    }
+
+    //------------------------------------------------------------
+    // Function is called once every frame
+    //------------------------------------------------------------
+    void Update()
+    {
+        // Calls Special function every frame
+        Special();
+    }
+
+    //------------------------------------------------------------
+    // Function allows Striker to use his regular attack
+    //------------------------------------------------------------
+    private void Special()
+    {
+        bool attackButton = XCI.GetButtonDown(XboxButton.RightBumper, Controller);
+
+        if (attackButton)
+        {
+            //int layerMask = 1 << LayerMask.NameToLayer("Enemy");
+            //CapsuleCollider box = HitBox.GetComponent<CapsuleCollider>();
+
+            //Collider[] hitEnemies = Physics.OverlapCapsule(HitBox.transform.position, box.radius * 0.5, HitBox.transform.rotation, layerMask);
+            //for (int i = 0; i < hitEnemies.Length; ++i)
+            //{
+            //    GameObject enemy = hitEnemies[i].gameObject;
+            //    Rigidbody rb = enemy.GetComponent<Rigidbody>();
+            //    NavMeshAgent agent = enemy.GetComponent<NavMeshAgent>();
+            //    BasicAIScript AI = enemy.GetComponent<BasicAIScript>();
+
+            //    AI.enabled = false;
+            //    agent.enabled = false;
+            //    rb.isKinematic = false;
+
+            //    Vector3 direction = enemy.transform.position - transform.position;
+            //    direction.Normalize();
+            //    rb.AddForce(direction * hitForce, ForceMode.Impulse);
+            //}
+        }
+    }
+}
