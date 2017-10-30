1b9bdc3680d186157494f0315ca463eb4bb8b293
Matthew Le Nepveu <matthew@lenepveu.com>
2017-10-30 12:26:17 +1100

    Added the EndLevel script to SVN and started work on it

---
+++ Unity/Project/TKB/Assets/ThumbsUp/Scripts/ProScripts/EndLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject closedDoor;
+    public GameObject openDoor;
+
+	// Use this for initialization
+	void Awake()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (enemies.Length <= 0)
+        {
+            Instantiate(openDoor, closedDoor.transform.position, closedDoor.transform.rotation);
+            Destroy(closedDoor);
+        }
+	}
+}
