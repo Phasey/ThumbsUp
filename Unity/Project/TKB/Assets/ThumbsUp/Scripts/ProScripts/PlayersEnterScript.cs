df8ac6de775655e4c16936db11ed0abae74bea0c
Scalarnax <lt.knights@live.com.au>
2017-11-14 15:12:28 +1100

    made the door spawning script after both players have entered the trigger

---
+++ Unity/Project/TKB/Assets/ThumbsUp/Scripts/ProScripts/PlayersEnterScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersEnterScript : MonoBehaviour {
+
+    public GameObject[] Players;
+    public GameObject DoorAfterEnter;
+
+    // Use this for initialization
+    void Awake()
+    {
+        Players = GameObject.FindGameObjectsWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == Players[0] && other == Players[1])
+        {
+            DoorAfterEnter.SetActive(true);
+        }
+    }
+}
