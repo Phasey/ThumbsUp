868ce2d0b1b8fe3b8d3dc75f1a7b4fd5302bf021
nbarnettdesign <nbarnett.design@gmail.com>
2017-11-21 14:34:02 +1100

    Hopefully Sweeper Fix

--- Unity/Project/TKB/Assets/ThumbsUp/Scripts/ProScripts/SweeperSpecial.cs
+++ Unity/Project/TKB/Assets/ThumbsUp/Scripts/ProScripts/SweeperSpecial.cs
@@ -7,171 +7,169 @@ using XboxCtrlrInput;
 
 public class SweeperSpecial : MonoBehaviour
 {
-    // Sets and initiases public floats so the designers can adjust them
-    public float hitForce = 10;
-    public float upForce = 0.5f;
-    public float radius = 5f;
-    public float damage = 5f;
-    public float coolDownTimerMax = 5f;
+	// Sets and initiases public floats so the designers can adjust them
+	public float hitForce = 10;
+	public float upForce = 0.5f;
+	public float radius = 5f;
+	public float damage = 5f;
+	public float coolDownTimerMax = 5f;
 	public AudioSource Spin;
 
-    // Creates a public powerbar so it can be set in unity
-    public Slider powerBar;
+	// Creates a public power bar so it can be set in unity
+	public Slider powerBar;
 
-    // Creates a public power bar colour so it can be set in unity
-    public Image powerBarColour;
+	// Creates a public power bar colour so it can be set in unity
+	public Image powerBarColour;
 
-    // Initialises the colour for when power bar is both full and not full
-    public Color powerFullColour = Color.yellow;
-    public Color powerNotFullColour = Color.blue;
+	// Initialises the colour for when power bar is both full and not full
+	public Color powerFullColour = Color.yellow;
+	public Color powerNotFullColour = Color.blue;
 
-    // Sets both a private bool to be false and a private float
-    private bool coolDown = false;
-    private float coolDownTimer;
+	// Sets both a private bool to be false and a private float
+	private bool coolDown = false;
+	private float coolDownTimer;
 
-    // Allows access to xbox controller buttons
-    private XboxController Controller;
+	// Allows access to xbox controller buttons
+	private XboxController Controller;
 
-    private Animator animator;
+	private Animator animator;
 
-    //------------------------------------------------------------
-    // Function is called when script first runs
-    //------------------------------------------------------------
-    void Awake()
-    {
-        // Gets the Player Move component
-        PlayerMove move = GetComponent<PlayerMove>();
-        animator = GetComponent<Animator>();
+	//------------------------------------------------------------
+	// Function is called when script first runs
+	//------------------------------------------------------------
+	void Awake()
+	{
+		// Gets the Player Move component
+		PlayerMove move = GetComponent<PlayerMove>();
+		animator = GetComponent<Animator>();
 
-        // Sets the controller to be the same as controller used in PlayerMove script
-        Controller = move.Controller;
+		// Sets the controller to be the same as controller used in PlayerMove script
+		Controller = move.Controller;
 
-        // Sets the cool down timer to equal the max cool down timer value
-        coolDownTimer = coolDownTimerMax;
+		// Sets the cool down timer to equal the max cool down timer value
+		coolDownTimer = coolDownTimerMax;
 
-        // Sets the power bar value to equal the max cool down timer value
-        powerBar.maxValue = coolDownTimerMax;
-    }
+		// Sets the power bar value to equal the max cool down timer value
+		powerBar.maxValue = coolDownTimerMax;
+	}
 
-    //------------------------------------------------------------
-    // Function is called once every frame
-    //------------------------------------------------------------
-    void Update()
-    {
-        // Calls Special function every frame
-        Special();
+	//------------------------------------------------------------
+	// Function is called once every frame
+	//------------------------------------------------------------
+	void Update()
+	{
+		// Calls Special function every frame
+		Special();
 
-        // Sets the power bar value to equal the value of the cool down timer
-        powerBar.value = coolDownTimer;
+		// Sets the power bar value to equal the value of the cool down timer
+		powerBar.value = coolDownTimer;
 
-        // If power bar value equals max, then make its colour equal the full colour
-        if (powerBar.value == coolDownTimerMax)
-            powerBarColour.color = powerFullColour;
+		// If power bar value equals max, then make its colour equal the full colour
+		if (powerBar.value == coolDownTimerMax)
+			powerBarColour.color = powerFullColour;
 
-        // Else, make the power bar's colour equal the not full colour
-        else
-            powerBarColour.color = powerNotFullColour;
-    }
+		// Else, make the powerbar's colour equal the not full colour
+		else
+			powerBarColour.color = powerNotFullColour;
+	}
 
-    //------------------------------------------------------------
-    // Function allows Striker to use his regular attack
-    //------------------------------------------------------------
-    private void Special()
-    {
-        // Bool checks if the right bumper has been pressed
-        float leftTrigger = XCI.GetAxis(XboxAxis.LeftTrigger, Controller);
-        bool attackButton = XCI.GetButtonDown(XboxButton.RightBumper, Controller);
-        bool yButton = XCI.GetButtonDown(XboxButton.Y, Controller);
-        bool attacking = animator.GetCurrentAnimatorStateInfo(0).IsName("Special") && !animator.IsInTransition(0);
+	//------------------------------------------------------------
+	// Function allows Striker to use his regular attack
+	//------------------------------------------------------------
+	private void Special()
+	{
+		// Bool checks if the right bumper has been pressed
+		bool attackButton = XCI.GetButtonDown(XboxButton.RightBumper, Controller);
+		bool yButton = XCI.GetButtonDown(XboxButton.Y, Controller);
 
-        // Checks if right bumper has been pressed down and sweeper is not in cool down
-        if (leftTrigger > 0.15 || attackButton || yButton)
-        {
-            if (!attacking)
-            {
-                animator.SetBool("Special", true);
+		// Checks if right bumper has been pressed down and sweeper is not in cool down
+		if (attackButton || yButton)
+		{
+			if (!coolDown)
+			{
+				animator.SetBool("Special", true);
+				if (!Spin.isPlaying && !coolDown) {
+					Spin.Play ();
+				}
 
-			    if (!Spin.isPlaying)
-                {
-				    Spin.Play();
-			    }
+				// Sets cool down bool to be true and sets timer to equal zero
+				coolDown = true;
+				coolDownTimer = 0f;
+			}
+		}
+		else
+			animator.SetBool("Special", false);
 
-                // Sets cool down bool to be true and sets timer to equal zero
-                //coolDown = true;
-                //coolDownTimer = 0f;
-            }
-        }
-        else
-            animator.SetBool("Special", false);
+		// Checks if cool down bool equals true
+		if (coolDown)
+		{
+			// Cool Down Timer is increased by deltaTime
+			coolDownTimer += Time.deltaTime;
 
-        // Checks if cool down bool equals true
-        //if (coolDown)
-        //{
-        //    // Cool Down Timer is increased by deltaTime
-        //    coolDownTimer += Time.deltaTime;
+			// If the timer exceeds or equals the max timer value, then set cool down back to false
+			if (coolDownTimer >= coolDownTimerMax)
+			{
+				coolDown = false;
+			}
+		}
 
-        //    // If the timer exceeds or equals the max timer value, then set cool down back to false
-        //    if (coolDownTimer >= coolDownTimerMax)
-        //    {
-        //        coolDown = false;
-        //    }
-        //}
+		HealthScript healthScript = GetComponent<HealthScript>();
 
-        HealthScript healthScript = GetComponent<HealthScript>();
+		bool specialInProgress = animator.GetCurrentAnimatorStateInfo(0).IsName("Special") && !animator.IsInTransition(0);
 
-        if (healthScript)
-            healthScript.inSpecial = attacking;
-    }
+		if (healthScript)
+			healthScript.inSpecial = specialInProgress;
+	}
 
-    void OnTriggerEnter(Collider other)
-    {
-        bool attacking = animator.GetCurrentAnimatorStateInfo(0).IsName("Special") && !animator.IsInTransition(0);
+	void OnTriggerEnter(Collider other)
+	{
+		bool attacking = animator.GetCurrentAnimatorStateInfo(0).IsName("Special") && !animator.IsInTransition(0);
 
-        if (other.tag != "Enemy")
-            return;
+		if (other.tag != "Enemy")
+			return;
 
-        if (!attacking)
-            return;
+		if (!attacking)
+			return;
 
-        // Stores current enemy as a GameObject
-        GameObject enemy = other.gameObject;
+		// Stores current enemy as a GameObject
+		GameObject enemy = other.gameObject;
 
-        // Gets the Rigidbody component of the current enemy
-        Rigidbody rb = enemy.GetComponent<Rigidbody>();
+		// Gets the Rigidbody component of the current enemy
+		Rigidbody rb = enemy.GetComponent<Rigidbody>();
 
-        // Gets the NavMeshAgent component of the current enemy
-        NavMeshAgent agent = enemy.GetComponent<NavMeshAgent>();
+		// Gets the NavMeshAgent component of the current enemy
+		NavMeshAgent agent = enemy.GetComponent<NavMeshAgent>();
 
-        // Gets the AI script of the current enemy
-        BasicAIScript AI = enemy.GetComponent<BasicAIScript>();
+		// Gets the AI script of the current enemy
+		BasicAIScript AI = enemy.GetComponent<BasicAIScript>();
 
-        // Disables the NavMeshAgent of the current enemy
-        agent.enabled = false;
+		// Disables the NavMeshAgent of the current enemy
+		agent.enabled = false;
 
-        // Disables kinematic of the enemy's Rigidbody
-        rb.isKinematic = false;
+		// Disables kinematic of the enemy's Rigidbody
+		rb.isKinematic = false;
 
-        // Vector3 determines the direction the enemy will fly and normalises the variable
-        Vector3 direction = enemy.transform.position - transform.position;
-        direction.Normalize();
+		// Vector3 determines the direction the enemy will fly and normalises the variable
+		Vector3 direction = enemy.transform.position - transform.position;
+		direction.Normalize();
 
-        // Adds force to make the enemy fly back
-        rb.AddForce(direction * hitForce + Vector3.up * upForce, ForceMode.Impulse);
+		// Adds force to make the enemy fly back
+		rb.AddForce(direction * hitForce + Vector3.up * upForce, ForceMode.Impulse);
 
-        // Decreases the enemies health by how much damage was dealt
-        AI.enemyHealth -= damage;
+		// Decreases the enemies health by how much damage was dealt
+		AI.enemyHealth -= damage;
 
-        // Checks if enemies health is equal to or goes below zero
-        if (AI.enemyHealth <= 0)
-        {
-            // If so, set the dead bool in AI script to be true for enemy 
-            AI.dead = true;
-        }
+		// Checks if enemies health is equal to or goes below zero
+		if (AI.enemyHealth <= 0)
+		{
+			// If so, set the dead bool in AI script to be true for enemy 
+			AI.dead = true;
+		}
 
-        else
-        {
-            agent.enabled = true;
-            rb.isKinematic = true;
-        }
-    }
+		else
+		{
+			agent.enabled = true;
+			rb.isKinematic = true;
+		}
+	}
 }
